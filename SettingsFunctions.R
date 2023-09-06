d953e0bd6e47d003e871147fef020d1d365ee3cd
Anthony Sena <asena5@its.jnj.com>
2023-09-06 11:29:06 -0400

    Updates for module release

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -54,7 +54,7 @@ createCharacterizationModuleSpecifications <- function(targetIds,
 
   specifications <- list(
     module = "CharacterizationModule",
-    version = "0.4.0-5",
+    version = "0.4.0",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = analysis
